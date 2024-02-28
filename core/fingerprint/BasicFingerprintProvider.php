857021e16936c2657af5b69c4cd4893a055246e5
Luka Spasojević <71207600+Lark3s@users.noreply.github.com>
2024-02-28 22:27:34 +0100

    Update BasicFingerprintProvider.php

--- core/fingerprint/BasicFingerprintProvider.php
+++ core/fingerprint/BasicFingerprintProvider.php
@@ -11,8 +11,8 @@
         //digitalni otisak je heš od heša user agenta + ip adrese
         public function provideFingerprint(): string
         {
-            $userAgent = filter_var($this->data['HTTP_USER_AGENT'] ?? '', FILTER_UNSAFE_RAW); // TODO: This one is also filter_unsafe_raw, like Session.php line 96
-            $ipAddress = filter_var($this->data['REMOTE_ADDR'] ?? '', FILTER_UNSAFE_RAW); // TODO: This one is also filter_unsafe_raw, like Session.php line 96, also I should think about not using IP address for fingerprints because how common it is to switch addresses nowadays
+            $userAgent = filter_var($this->data['HTTP_USER_AGENT'] ?? '', FILTER_UNSAFE_RAW); 
+            $ipAddress = filter_var($this->data['REMOTE_ADDR'] ?? '', FILTER_UNSAFE_RAW); 
             $string = $userAgent . '|' . $ipAddress;
             $hash1 = hash('sha512', $string);
             return hash('sha512', $hash1);
